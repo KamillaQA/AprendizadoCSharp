98bd0d9450f96abce82fbaca165a2f7662832f1e
Kamilla Prado <kprado@websupply.com.br>
2025-03-12 11:32:28 -0300

    Iniciando o projeto com um desafio

---
+++ AprendizadoCSharp/ProjetoCSharp/DesafioCurso/Curso.cs
@@ -0,0 +1,18 @@
+﻿namespace DesafioCurso
+{
+    public class Curso
+    {
+        public string CursoEstudado { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Instrutores { get; set; }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine("Curso: " + CursoEstudado);
+            Console.WriteLine("Nome: " + Nome);
+            Console.WriteLine("Sobrenome: " + Sobrenome);
+            Console.WriteLine("Instrutores: " + Instrutores);
+        }
+    }
+}
---
+++ AprendizadoCSharp/ProjetoCSharp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using DesafioCurso; // Namespace para o desafio
+
+class Program
+{
+    static void Main()
+    {
+        // Mensagem de boas-vindas ao projeto Screen Sound
+        string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+        Console.WriteLine(mensagemDeBoasVindas);
+
+        // Executar o desafio do curso
+        DesafioCurso();
+
+        // Aguarda o usuário pressionar Enter antes de fechar o console
+        Console.ReadLine();
+    }
+
+    static void DesafioCurso()
+    {
+        // Criando e exibindo as informações do curso
+        Curso curso = new Curso
+        {
+            CursoEstudado = "C#",
+            Nome = "Kamilla",
+            Sobrenome = "Prado",
+            Instrutores = "Instrutor A, Instrutor B"
+        };
+
+        curso.ExibirInformacoes();
+    }
+}
