a1a4bea47be90764f1c968fadf6514677c03f8ab
Kamilla Prado <kprado@websupply.com.br>
2025-03-12 12:37:59 -0300

    Adicionando desafio e listas/loops no C#

--- AprendizadoCSharp/ProjetoCSharp/Program.cs
+++ AprendizadoCSharp/ProjetoCSharp/Program.cs
@@ -10,18 +10,10 @@ class Program
         string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
         List<string> listaDasBandas = new List<string> { "Maverick", "Upperoom", "Adele" };
 
-        ExibirMensagemDeBoasVindas(mensagemDeBoasVindas);
         ExibirOpcoesDoMenu();
 
         // Aguarda o usuário pressionar Enter antes de fechar o console
         Console.ReadLine();
-
-        static void ExibirMensagemDeBoasVindas(string mensagem)
-        {
-            Console.WriteLine("**************************");
-            Console.WriteLine(mensagem);
-            Console.WriteLine("**************************");
-        }
 
         void ExibirLogo()
         {
@@ -32,6 +24,44 @@ class Program
             ░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
             ██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
             ╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+            Console.WriteLine(mensagemDeBoasVindas);
+        }
+        void RegistrarBanda()
+        {
+            Console.Clear();
+            Console.WriteLine("**********************");
+            Console.WriteLine("Registro de bandas");
+            Console.WriteLine("**********************\n");
+            Console.Write("Digite o nome da banda que deseja registrar: ");
+            string nomeDaBanda = Console.ReadLine()!;
+            listaDasBandas.Add(nomeDaBanda);
+            Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
+            Thread.Sleep(2000);
+            Console.Clear();
+            ExibirOpcoesDoMenu();
+        }
+        void MostrarBandasRegistradas()
+        {
+            Console.Clear();
+            Console.WriteLine("************************************");
+            Console.WriteLine("Exibindo todas as bandas registradas");
+            Console.WriteLine("************************************\n");
+
+            //for (int i = 0; i < listaDasBandas.Count; i++)
+            //{
+            //Console.WriteLine($"Banda: {listaDasBandas[i]}");
+            //}
+
+            foreach (string banda in listaDasBandas)
+            {
+                Console.WriteLine($"Banda: {banda}");
+            }
+
+            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+            ExibirOpcoesDoMenu();
+
         }
         void ExibirOpcoesDoMenu()
         {
@@ -49,10 +79,10 @@ class Program
             switch (opcaoEscolhidaNumerica)
             {
                 case 1:
-                    Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+                    RegistrarBanda();
                     break;
                 case 2:
-                    Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+                    MostrarBandasRegistradas();
                     break;
                 case 3:
                     Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
@@ -68,71 +98,127 @@ class Program
                     break;
             }
         }
-       
-    }
+        /*1 desafio sobre C#
+     static void DesafioCurso()
+     {
+         // Criando e exibindo as informações do curso
+         Curso curso = new Curso
+         {
+             CursoEstudado = "C#",
+             Nome = "Kamilla",
+             Sobrenome = "Prado",
+             Instrutores = "Instrutor A, Instrutor B"
+         };
 
-    /*Primeiro desafio sobre C#
-    static void DesafioCurso()
-    {
-        // Criando e exibindo as informações do curso
-        Curso curso = new Curso
+         curso.ExibirInformacoes();
+     }
+       // Executar o desafio do curso
+         DesafioCurso();
+     */
+
+
+        /*2 desafio: Função para verificar a nota e exibir uma mensagem
+        static void VerificarAprovacao()
         {
-            CursoEstudado = "C#",
-            Nome = "Kamilla",
-            Sobrenome = "Prado",
-            Instrutores = "Instrutor A, Instrutor B"
-        };
-
-        curso.ExibirInformacoes();
-    }
-      // Executar o desafio do curso
-        DesafioCurso();
-    */
-
-
-    /*Segundo desafio de C#
-    // Função para verificar a nota e exibir uma mensagem
-    static void VerificarAprovacao()
-    {
-        int notaMedia = 6; // Exemplo: atribuímos 6 para a nota
-        if (notaMedia >= 5)
-        {
-            Console.WriteLine("Nota suficiente para aprovação");
+            int notaMedia = 6; // Exemplo: atribuímos 6 para a nota
+            if (notaMedia >= 5)
+            {
+                Console.WriteLine("Nota suficiente para aprovação");
+            }
+            else
+            {
+                Console.WriteLine("Nota insuficiente para aprovação");
+            }
         }
-        else
+
+        // Função para exibir uma linguagem de programação da lista
+        static void ExibirLinguaDeProgramacao()
         {
-            Console.WriteLine("Nota insuficiente para aprovação");
+            List<string> linguagensDeProgramacao = new List<string> { "C#", "Java", "JavaScript" };
+            Console.WriteLine("Linguagem exibida: " + linguagensDeProgramacao[0]); // Exibe "C#"
         }
+
+        // Função para exibir a n-ésima posição da lista
+        static void ExibirPosicaoLista()
+        {
+            List<string> linguagensDeProgramacao = new List<string> { "C#", "Java", "JavaScript" };
+
+            Console.Write("\nDigite um número inteiro n para acessar a n-ésima posição da lista (começando de 0): ");
+            int n = int.Parse(Console.ReadLine()!);
+
+            // Verificar se n está dentro dos limites da lista
+            if (n >= 0 && n < linguagensDeProgramacao.Count)
+            {
+                Console.WriteLine($"O valor na posição {n} é: {linguagensDeProgramacao[n]}");
+            }
+            else
+            {
+                Console.WriteLine("Número fora do alcance da lista.");
+            }
+        }
+          // Executar o desafio do C#
+           VerificarAprovacao();
+           ExibirLinguaDeProgramacao();
+           ExibirPosicaoLista();
+        */
+
+        //3 desafio: Função para exibir as quatro operações básicas
+        /*static void ExibirOperacoesBasicas(double a, double b)
+        {
+            Console.WriteLine($"Adição: {a} + {b} = {a + b}");
+            Console.WriteLine($"Subtração: {a} - {b} = {a - b}");
+            Console.WriteLine($"Multiplicação: {a} * {b} = {a * b}");
+
+            if (b != 0) // Verificando para não dividir por zero
+            {
+                Console.WriteLine($"Divisão: {a} / {b} = {a / b}");
+            }
+            else
+            {
+                Console.WriteLine("Divisão: Não é possível dividir por zero");
+            }
+        }
+        ExibirOperacoesBasicas(5.5, 2.2);*/
+
+        //4 desafio: Criar uma lista de bandas vazia e adicionar suas bandas preferidas 
+        /* List<string> bandasPreferidas = new List<string>();
+
+         // Adicionando as bandas favoritas
+         bandasPreferidas.Add("Coldplay");
+         bandasPreferidas.Add("Imagine Dragons");
+         bandasPreferidas.Add("The Beatles");
+
+         // Chamando a função para mostrar todas as bandas
+         MostrarBandas(bandasPreferidas);
+         static void MostrarBandas(List<string> bandas)
+         {
+             Console.WriteLine("Minhas bandas favoritas:");
+
+             // Usando o 'for' para exibir todas as bandas
+             for (int i = 0; i < bandas.Count; i++)
+             {
+                 Console.WriteLine(bandas[i]);
+             }
+         }*/
+
+        //5 desafio: Calcular a soma de todos os elementos inteiros em uma lista
+        /*List<int> numeros = new List<int> { 1, 2, 3, 4, 5 };
+
+        // Calculando e exibindo a soma
+        int soma = CalcularSoma(numeros);
+        Console.WriteLine($"A soma de todos os elementos é: {soma}");
+        static int CalcularSoma(List<int> lista)
+        {
+            int soma = 0;
+
+            // Somando todos os elementos da lista
+            for (int i = 0; i < lista.Count; i++)
+            {
+                soma += lista[i];
+            }
+
+            return soma;
+        }*/
     }
 
-    // Função para exibir uma linguagem de programação da lista
-    static void ExibirLinguaDeProgramacao()
-    {
-        List<string> linguagensDeProgramacao = new List<string> { "C#", "Java", "JavaScript" };
-        Console.WriteLine("Linguagem exibida: " + linguagensDeProgramacao[0]); // Exibe "C#"
-    }
-
-    // Função para exibir a n-ésima posição da lista
-    static void ExibirPosicaoLista()
-    {
-        List<string> linguagensDeProgramacao = new List<string> { "C#", "Java", "JavaScript" };
-
-        Console.Write("\nDigite um número inteiro n para acessar a n-ésima posição da lista (começando de 0): ");
-        int n = int.Parse(Console.ReadLine()!);
-
-        // Verificar se n está dentro dos limites da lista
-        if (n >= 0 && n < linguagensDeProgramacao.Count)
-        {
-            Console.WriteLine($"O valor na posição {n} é: {linguagensDeProgramacao[n]}");
-        }
-        else
-        {
-            Console.WriteLine("Número fora do alcance da lista.");
-        }
-    }
-      // Executar o desafio do C#
-       VerificarAprovacao();
-       ExibirLinguaDeProgramacao();
-       ExibirPosicaoLista();
-    */
 }
--- AprendizadoCSharp/obj/Debug/net8.0/AprendizadoCSharp.AssemblyInfo.cs
+++ AprendizadoCSharp/obj/Debug/net8.0/AprendizadoCSharp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AprendizadoCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98bd0d9450f96abce82fbaca165a2f7662832f1e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+665eb3316004465c1b41585f287e08dbcb085e31")]
 [assembly: System.Reflection.AssemblyProductAttribute("AprendizadoCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AprendizadoCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
